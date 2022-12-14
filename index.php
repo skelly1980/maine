f589751054cfa76ead9be1258dfa4822fcd0c3ea
skelly1980 <118386240+skelly1980@users.noreply.github.com>
2022-12-13 23:48:29 -0500

    initisl commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Healthcare Training for Me</title>
+    <link rel="stylesheet" href="public/css/style.css">
+    <script src="https://kit.fontawesome.com/bb55158e46.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header class="header">
+        <?php include "src/includes/header.php" ?>
+    </header>
+    <main class="main">
+        <div class="container">
+            <div class="column">
+                <div class="intro">
+                    <h1 class="text-center">Healthcare Training For ME</h1>
+                </div>
+            </div>
+            <div class="row text-center">
+                <h3><strong>Connect to healthcare training resources to accelerate
+                        <br />
+                        your career or upskill your current workforce</strong>
+                </h3>
+                <p>Healthcare Training for ME is a statewide collaboration of educational institutions and government agency partners convened to ensure Maine’s workers and employers can easily access healthcare training opportunities and supports. Funding for Training for ME
+                    is provided through the <a href="#" target="-blank">Maine Jobs & Recovery Plan</a> to advance Maine's workforce and economic goals, with additional funds provided by the <a href="#" target="-blank">Harold Alfond Foundation</a>.</p>
+            </div>
+            <div class="row">
+                <div class="container">
+                    <a href="#" target="_blank"><button class="hero-button">For Learners: Training for You</button></a>
+                    <a href="#" target="_blank"><button class="hero-button">For Employers: Training Your Workforce</button></a>
+                    <a href="#" target="_blank"><button class="hero-button">Frequently Asked Questions</button></a>
+                </div>
+            </div>
+        </div>
+
+        <section class="training-section">
+            <div class="container padding-6">
+                <div class="section-column-1">
+                    <div class="section-row">
+                        <h2>Training Resources</h2>
+                        <h3>For learners: Training for you</h3>
+                        <p>There are many healthcare training options available to you in Maine—see below for different types of trainings that might meet your needs:</p>
+                    </div>
+                </div>
+                <div class="section-column-2 flex">
+                    <div class="section-row-1 padding-6 ">
+                        <h3>Free Trainings and Earn While You learn:</h3>
+                        <p>Learn more about how you can access FREE trainings today and opportunities that allow you to earn while you learn on the job.</p>
+                        <h3>Short-term (1 year or less) and Mid-term (1-2 years):</h3>
+                        <p>Maine Community College System (MCCS) and Maine Adult Education offer short-term healthcare trainings ranging from 3 weeks to 1 year, with online and in-person options that allow for flexibility.</p>
+                        <p>Programs that take 1-2 years to complete offer advanced credentials and accelerated career pathways—offered by Maine Adult Education, Maine Community College System (MCCS), and the University of Maine System (UMS).</p>
+                        <h3>Longer-term and Degree Programs:</h3>
+                        <p>Looking to participate in a degree program or go back to school full time for the next 2+ years? The universities of the University of Maine System and the Maine Community College System have nursing and other degree programs that might be for you — learn more here!</p>
+                        <a href="#" target="_blank"><button class="hero-button">Upcoming Training</button></a>
+                        <p>Not sure which program or training is a good fit?
+                            Questions about how to pay for training?</p>
+                        <p>Tell us more about you and we can help connect you to opportunities that best fit your needs.</p>
+                        <a href="#" target="_blank"><button class="hero-button">Individual Training Request</button></a>
+                    </div>
+                    <div class="section-row-2 padding-6">
+                        <h3>Priority Job Trainings</h3>
+                        <p>Based on current needs in Maine, the following occupational trainings are highest priority to receive partial or full funding: </p>
+                        <ul>
+                            <li>ACRE Basic Employment Certificate Training </li>
+                            <li>Advanced Emergency Medical Technician (AEMT) </li>
+                            <li>Behavioral Health Professional (BHP)</li>
+                            <li>Certified Clinical Supervisor</li>
+                            <li>Certified Counseling Aide</li>
+                            <li>Certified Dementia Care Provider</li>
+                            <li>Certified Intentional Peer Support </li>
+                            <li>Certified Nursing Assistant (CNA)</li>
+                            <li>CNA-M</li>
+                            <li>CNA to LPN</li>
+                            <li>Certified Residential Medication Aide (CRMA) </li>
+                            <li>CIC Infection Preventionist certification</li>
+                            <li>Dental assistant </li>
+                            <li>Direct support professional (DSP) </li>
+                            <li>Emergency Medical Technician (EMT)</li>
+                            <li>Expanded Functions Dental Assistant</li>
+                            <li>Home Health Aide</li>
+                            <li>Medical Assistant (MA)</li>
+                            <li>Mental Health Rehabilitation Technician-1 (MHRT-1)&(MHRT-C) </li>
+                            <li>Paramedic (accelerated)</li>
+                            <li>Patient service representative (PSR)</li>
+                            <li>Personal Support Specialist</li>
+                            <li>Phlebotomist</li>
+                            <li>Recovery Coach</li>
+                            <li>Registered Behavior Technician (RBT)</li>
+                            <li>Surgical Technologist (accelerated)</li>
+                        </ul>
+                        <p>Not sure which job is for you or need more support on your career & educational journey?</p>
+                        <ul>
+                            <li>Explore healthcare career pathways (PDF)</li>
+                            <li>Learn more about other programs to support your educational and career advancement journey including HOPE, CSSP, and loan repayment options from HRSA and FAME!</li>
+                            <li>Connect with a CareerCenter counselor via Live Chat</li>
+                            <li>Learn more about how your previous education or experience can count toward earning a new credential through resources offered through at New Mainers Resource Center and UMaine System</li>
+                        </ul>
+                        <div class="section-row-2-image">
+                            <img src="src/scss/pages/assets/med-team.jpg" alt="medical team">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <h1>goodbye</h1>
+</body>
+
+</html>
---
+++ src/includes/header.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <nav class="navbar flex">
+        <a href="#" target="blank"><img src="public/assets/maine.png" alt="Maine.gov logo">
+            <ul class="flex list-style">
+                <li><a href="#" target="blank">Agencies</a></li>
+                <li><a href="#" target="blank">Online Services</a></li>
+                <li><a href="#" target="blank">Help</a></li>
+                <li><a href="#" target="blank"><i class="fa-solid fa-magnifying-glass"></i>Search Maine.gov</a></li>
+            </ul>
+    </nav>
+</div>
